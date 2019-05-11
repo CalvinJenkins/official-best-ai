bbe5a33deb4b60592c975fa4e4f5ef553b411b5f
root <root@LAPTOP-C22L9TMV.localdomain>
2019-05-11 11:26:17 -0700

    First File

---
+++ thoughts.py
@@ -0,0 +1,11 @@
+from colorfight import Colorfight
+import time
+import random
+from colorfight.constants import BLD_GOLD_MINE, BLD_ENERGY_WELL, BLD_FORTRESS
+
+#Important Concepts:
+#1:Protect Home Base
+#2:Build on Cells for best return
+#3:attack cells with possibility of best return
+#4:know when near other players, respond accordingly
+#5:Maaaaaaybeee:attack home bases
